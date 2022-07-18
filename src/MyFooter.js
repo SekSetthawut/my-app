5191deed59b03848287b1a6b2aff8bd352808347
SekSetthawut <setthawut.wa.65@ubu.ac.th>
2022-07-18 10:59:21 +0700

    Add Fi

---
+++ src/MyFooter.js
@@ -0,0 +1,10 @@
+import './App.css';
+function MyFooter(){
+    return (
+      <div className='My-footer'>
+        <span>Developed by Setthawut @ 2022</span>
+      </div>
+    );
+  }
+
+  export default MyFooter;
